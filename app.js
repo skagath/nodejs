5933d9f1ca87843898aa50449a520749d212f49c
skagath <155049450+skagath@users.noreply.github.com>
2025-03-07 11:45:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from bitcot!...');
+  res.send('this is the sample application!...');
 });
 
 app.listen(port, () => {
